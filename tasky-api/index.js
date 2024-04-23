706416b0d440e47c5850f96fdeefe15163043853
Juliana <m17juliana@gmail.com>
2024-04-23 09:47:13 +0100

    Routes for get task and task details

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 const port = process.env.PORT;
 
-app.use('/api/tasks/', tasksRouter);
+app.use('/api/tasks', tasksRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
